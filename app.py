56c73ed29661bbea0a1fd7458ed7fd89bdc14172
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-10 16:28:03 +0300

    ft:create a route for registration form

--- app.py
+++ app.py
@@ -34,6 +34,14 @@ def about():
     return render_template('about.html',title ='About')
 
 
+@app.route("/register")
+
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title ='Register', form= form)
+
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
