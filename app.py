639ed913b7d3a5b179a4d3de866eb2a4ff80ef41
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-10 13:08:09 +0300

    st:added bootstrap for design

--- app.py
+++ app.py
@@ -2,18 +2,34 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+
+posts = [
+    {
+        'author':'Gabriel Ndolo',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'May 2,2022'
+    },
+    {
+        'author':'Gabriella Heinze',
+        'title':'Blog post 2',
+        'content':'Second post content',
+        'date_posted':'June 6,2022'
+    }
+]
+
 @app.route("/")
 
 @app.route("/home")
 
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts = posts)
 
 
 @app.route("/about")
 
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title ='About')
 
 
 
