fa867bb315f0fe7307561943ce3702d518b2bfee
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-11 13:01:56 +0300

    st:created a structure and moved all files and folders

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from forms import validators
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '76bfd1aa0261f9781a899f00d901908c'
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
 
 posts = [
     {
